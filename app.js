269716bbdb546ac65e85d7dbcca7c25634781e3f
jsir99 <jameslindictates@gmail.com>
2021-09-05 21:35:17 -0500

    save2-missingValidators

---
+++ app.js
@@ -0,0 +1,35 @@
+const billInput = document.querySelector('#billform input')
+const tipOptions = document.getElementsByName('tip-percent')
+const numberPeopleInput = document.querySelector('#number-people input')
+const tipSpan = document.querySelector('#tip-amount .amount')
+const totalSpan = document.querySelector('#total .amount')
+const resetBtn = document.querySelector('#reset button')
+
+let bill = 0;
+let tip = 0.15;
+let numberPeople = 1;
+
+function updateMaths(){
+    let tipAmountPP = bill*tip/numberPeople;
+    let totalPP = bill*(1+tip)/numberPeople;
+    tipSpan.innerText = `$${tipAmountPP.toFixed(2)}`
+    totalSpan.innerText = `$${totalPP.toFixed(2)}`
+}
+
+function updateTip(e){
+    tip = parseFloat(this.value)
+    updateMaths()
+}
+tipOptions.forEach(tipOption => tipOption.addEventListener('change', updateTip));
+
+billInput.addEventListener('change', (e) =>{
+    bill = parseFloat(billInput.value);
+    updateMaths()
+})
+
+numberPeopleInput.addEventListener('change', (e)=>{
+    numberPeople = parseInt(numberPeopleInput.value)
+    updateMaths()
+})
+
+resetBtn.addEventListener('click', (e)=>location.reload())
