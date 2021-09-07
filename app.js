48f8aa84d49b36498e33d85ce5110aa9d98b174d
jsir99 <jameslindictates@gmail.com>
2021-09-06 19:26:05 -0500

    sol-1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const billInput = document.querySelector('#billform input')
 const tipOptions = document.getElementsByName('tip-percent')
+const numberPeopleForm = document.querySelector('#numberpeopleform')
 const numberPeopleInput = document.querySelector('#number-people input')
+const errorMsg = document.querySelector('#numberpeopleform > span')
 const tipSpan = document.querySelector('#tip-amount .amount')
 const totalSpan = document.querySelector('#total .amount')
 const resetBtn = document.querySelector('#reset button')
@@ -15,6 +17,11 @@ function updateMaths(){
     tipSpan.innerText = `$${tipAmountPP.toFixed(2)}`
     totalSpan.innerText = `$${totalPP.toFixed(2)}`
 }
+function toggleError(){
+    numberPeopleForm.classList.toggle("error")
+    errorMsg.classList.toggle("error-msg")
+}
+
 
 function updateTip(e){
     tip = parseFloat(this.value)
@@ -22,12 +29,20 @@ function updateTip(e){
 }
 tipOptions.forEach(tipOption => tipOption.addEventListener('change', updateTip));
 
-billInput.addEventListener('change', (e) =>{
-    bill = parseFloat(billInput.value);
-    updateMaths()
+billInput.addEventListener('keyup', (e) =>{
+        bill = parseFloat(billInput.value);
+        updateMaths()
 })
 
-numberPeopleInput.addEventListener('change', (e)=>{
+numberPeopleInput.addEventListener('change', function(e){
+    if(parseInt(this.value) === 0){
+     toggleError()
+     return;   
+    }
+    if (numberPeopleForm.classList.contains('error')) {
+        numberPeopleForm.classList.toggle('error')
+        errorMsg.classList.toggle('error-msg')
+    }
     numberPeople = parseInt(numberPeopleInput.value)
     updateMaths()
 })
